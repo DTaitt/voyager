5efd52ad45d8beea0b3202b154827d9bfc9f93b3
Dtaitt <Donovan.Taitt@yahoo.com>
2018-09-11 19:36:40 -0400

    added semantic ui

---
+++ client/src/components/App/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import './App.css';
+import { Button } from 'semantic-ui-react'
+
+export default function App() {
+  return (
+    <Button>I'm a button on the home page</Button>
+  )
+}
